776fc680c8ee0807fc4c4c7bc29b4994beb2fae8
deput <canopus4u@outlook.com>
2016-03-06 19:08:12 +0800

    init

---
+++ ObjC/RWTaskKitDemo.ObjC/RWTaskKitDemo.ObjC/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  RWTaskKitDemo.ObjC
+//
+//  Created by deput on 3/6/16.
+//  Copyright (c) 2016 RW. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
---
+++ ObjC/RWTaskKitDemo.ObjC/RWTaskKitDemo.ObjC/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  RWTaskKitDemo.ObjC
+//
+//  Created by deput on 3/6/16.
+//  Copyright (c) 2016 RW. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController : UIViewController
+
+
+@end
